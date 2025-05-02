c60ef0e97dcb2dfaa582d6f40c3e754ed79c755d
Luis Schubert <schubert.luis@gmail.com>
2025-05-02 14:24:31 -0700

    lock

--- flake.nix
+++ flake.nix
@@ -1,27 +1,25 @@
-{ description = (
+{
+  description = (
     "i.MX (8) specific Linux kernel, u-boot bootloader, firmware, etc packaged in Nix for NixOS"
-    /**
-     * This flake file defines the main inputs (all except for some files/archives fetched by hardcoded hash) and exports almost all usable results.
-     * It should always pass »nix flake check« and »nix flake show --allow-import-from-derivation«, which means inputs and outputs comply with the flake convention.
-     */
-); inputs = {
+  );
 
-    # To update »./flake.lock«: $ nix flake update
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.05"; };
-    functions = { url = "github:NiklasGollenstede/nix-functions"; inputs.nixpkgs.follows = "nixpkgs"; };
-    installer = { url = "github:NiklasGollenstede/nixos-installer"; inputs.nixpkgs.follows = "nixpkgs"; inputs.functions.follows = "functions"; };
-    wiplib = { url = "github:NiklasGollenstede/nix-wiplib"; inputs.nixpkgs.follows = "nixpkgs"; inputs.functions.follows = "functions"; inputs.installer.follows = "installer"; };
-    nixos-imx = { url = "github:NiklasGollenstede/nixos-imx"; inputs.nixpkgs.follows = "nixpkgs"; inputs.wiplib.follows = "wiplib"; }; # (uncomment this when reusing this file in your own repo)
-    # nixos-imx = { url = "github:luisschubert/nixos-imx"; inputs.nixpkgs.follows = "nixpkgs"; inputs.wiplib.follows = "wiplib"; };
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/6da4bc6cb07cba1b8e53d139cbf1d2fb8061d967"; };
+    functions = { url = "github:NiklasGollenstede/nix-functions/d4249dd208e055bfdf43a249894dabcac7ea8b24"; inputs.nixpkgs.follows = "nixpkgs"; };
+    installer = { url = "github:NiklasGollenstede/nixos-installer/6a886a839df96ea37b3b256d5ea2fbbe549d68a8"; inputs.nixpkgs.follows = "nixpkgs"; inputs.functions.follows = "functions"; };
+    wiplib = { url = "github:NiklasGollenstede/nix-wiplib/b0587d12a6f9dab63c2023fd8ced3560a9e89d6a"; inputs.nixpkgs.follows = "nixpkgs"; inputs.functions.follows = "functions"; inputs.installer.follows = "installer"; };
+    # nixos-imx = { url = "github:luisschubert/nixos-imx/1d42b7865e4036a3a0a3115ed47e91d8d7049064"; inputs.nixpkgs.follows = "nixpkgs"; inputs.wiplib.follows = "wiplib"; };
+  };
 
-}; outputs = inputs@{ self, ... }: inputs.functions.lib.importRepo inputs ./. (repo@{ overlays, ... }: let
+  outputs = inputs@{ self, ... }: inputs.functions.lib.importRepo inputs ./. (repo@{ overlays, ... }: let
     lib = inputs.nixpkgs.lib // { fun = inputs.functions.lib; inst = inputs.installer.lib; wip = inputs.wiplib.lib; };
-in [ # Run »nix flake show --allow-import-from-derivation« to see what this merges to:
-    repo { lib.__internal__ = lib; } # => lib overlays.* nixosModules.*
-    (lib.inst.mkSystemsFlake { inherit inputs; }) # => nixosConfigurations.* packages.*-linux.all-systems
-    (lib.inst.mkSystemsFlake { inherit inputs; buildPlatform = "x86_64-linux"; renameOutputs = key: "x64:${key}"; }) # => nixosConfigurations.x64:* packages.*-linux.x64:all-systems
+  in [
+    repo { lib.__internal__ = lib; }
+    (lib.inst.mkSystemsFlake { inherit inputs; })
+    (lib.inst.mkSystemsFlake { inherit inputs; buildPlatform = "x86_64-linux"; renameOutputs = key: "x64:${key}"; })
     (lib.fun.forEachSystem [ "aarch64-linux" "x86_64-linux" ] (localSystem: {
-        packages = lib.fun.getModifiedPackages (lib.fun.importPkgs inputs { system = localSystem; }) overlays;
-        defaultPackage = self.packages.${localSystem}.all-systems;
+      packages = lib.fun.getModifiedPackages (lib.fun.importPkgs inputs { system = localSystem; }) overlays;
+      defaultPackage = self.packages.${localSystem}.all-systems;
     }))
-]); }
+  ]);
+}
